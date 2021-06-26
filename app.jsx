79a9d528818f1731f4d7dd94f63939fb5d36be88
Chintamanikhair13 <86404944+Chintamanikhair13@users.noreply.github.com>
2021-06-26 06:28:18 -0800

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./header"
+import Footer from "./footer"
+import Info from "./info"
+
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+      <Info/>
+      <Info/>
+      <Info/>
+      <Info/>
+      <Info/>
+      <Info/>
+      <Info/>
+      <Info/>
+
+    </div>
+  );
+
+}
+
+export default App;
+
---
+++ info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Info(){
+  return (
+
+    <div class="note">
+      <h1>full stack development</h1>
+      <p>basic course on full stack dvelopment</p>
+    </div>
+  );
+
+}
+
+export default Info;
